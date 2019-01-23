299f8df1deceb667245eadc4b5d44539d4cd3710
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-01-23 20:56:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 class Application < Sinatra::Base
+<<<<<<< HEAD
   get '/' do
     erb :index
   end
 
   post '/greet' do 
   	erb :greet 
+=======
+  get '/' do 
+    erb :index 
+>>>>>>> efdfbfe08e11dfddbda374bed4d12cd0574471ba
   end
 
 end
